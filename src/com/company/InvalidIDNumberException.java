83850da9bd9b58319264a52678603178e6918723
Brian B <brianb@MacBook-Pro-4.local>
2019-02-04 11:02:23 -0500

    Adds InvalidIDNumberException class with constructor

--- src/com/company/InvalidIDNumberException.java
+++ src/com/company/InvalidIDNumberException.java
@@ -1,4 +1,8 @@
 package com.company;
 
-public class InvalidIDNumberException {
+public class InvalidIDNumberException extends Exception {
+
+    public InvalidIDNumberException(String message){
+        super(message);
+    }
 }
